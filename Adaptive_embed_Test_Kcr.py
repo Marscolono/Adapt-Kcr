e9c7917c0ae5b2700bded37b5bef1d1dc18c8bc7
Marscolono <30385448+Marscolono@users.noreply.github.com>
2022-01-13 20:18:48 +0800

    Update Adaptive_embed_Test_Kcr.py

--- Adaptive_embed_Test_Kcr.py
+++ Adaptive_embed_Test_Kcr.py
@@ -348,11 +348,11 @@ if __name__ == '__main__':
 	device_cpu = torch.device("cpu") 
 	# wordvec_len =4
 	
-	k_folds = k
-	EPOCH = epoch
-	BATCH_SIZE = bs
-	LR = lr 
-	kernel_size = ks
+	k_folds = 10
+	EPOCH = 50
+	BATCH_SIZE = 256
+	LR = 0.01 
+	kernel_size = 10
 	
 	#!!! Model
 
